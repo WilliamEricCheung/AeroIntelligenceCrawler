11222aafdd880e9be737b4e6b3505815cb3af6ec
Yihui Zhang <zhangyihui@buaa.edu.cn>
2024-03-15 16:12:10 +0800

    add install-prerequisite script

---
+++ install-prerequisite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# usage: echo [root-password] | sudo -S ./install-prerequisite.sh
+
+# install browswer driver (chrome for ubuntu)
+echo "****************************
+   Step 1: Install Chrome
+****************************"
+
+if [ ! -f google-chrome-stable_current_amd64.deb ]; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+fi
+sudo apt-get install libu2f-udev
+sudo dpkg -i google-chrome-stable_current_amd64.deb
