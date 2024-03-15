68b9e6a3363025efb57a790fb16d9792da286a33
QiwenZhou <452252441@qq.com>
2024-03-15 23:05:31 +0800

    update requirement

--- install-prerequisite.sh
+++ install-prerequisite.sh
@@ -16,3 +16,18 @@ sudo apt-get install libu2f-udev
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install
 # default chrome path: /opt/google/chrome/
+
+# install python3
+sudo apt-get update
+sudo apt-get install python3
+
+sudo apt-get install libxml2-devel libxslt-devel
+
+cd AeroIntelligenceCrawler
+# create virtual environment
+python3 -m venv crawler
+# activate virtual environment
+source crawler/bin/activate
+# install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
