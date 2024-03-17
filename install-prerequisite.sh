1963772d17c1ff284be3d1bd9c59b804aee1b721
QiwenZhou <452252441@qq.com>
2024-03-17 09:33:07 +0800

    Install Chrome on a Debian-based system

--- install-prerequisite.sh
+++ install-prerequisite.sh
@@ -8,20 +8,35 @@ echo "
    Step 1: Install Chrome
 ****************************
 "
-
-if [ ! -f google-chrome-stable_current_amd64.deb ]; then
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+if grep -q -i "debian" /etc/os-release; then
+    echo "This is a Debian-based system."
+    if [ ! -f google-chrome-stable_current_amd64.deb ]; then
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    fi
+    sudo apt-get install libu2f-udev
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt-get -f install
+    # default chrome path: /opt/google/chrome/
+elif grep -q -i "rhel fedora" /etc/os-release; then
+    echo "This is a RPM-based system."
+    if [ ! -f google-chrome-stable_current_x86_64.rpm ]; then
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    fi
+    sudo yum install libu2f-udev
+    sudo rpm -i google-chrome-stable_current_x86_64.rpm
+    sudo yum -y install
+else
+    echo "Unsupported operating system."
 fi
-sudo apt-get install libu2f-udev
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get -f install
-# default chrome path: /opt/google/chrome/
 
+echo "
+**************************************************
+   Step 2: Install Python and Related Dependencies
+**************************************************
+"
 # install python3
 sudo apt-get update
 sudo apt-get install python3
-
-sudo apt-get install libxml2-devel libxslt-devel
 
 cd AeroIntelligenceCrawler
 # create virtual environment
@@ -29,5 +44,6 @@ python3 -m venv crawler
 # activate virtual environment
 source crawler/bin/activate
 # install dependencies
+sudo apt-get install libxml2-devel libxslt-devel
 pip install --upgrade pip
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
