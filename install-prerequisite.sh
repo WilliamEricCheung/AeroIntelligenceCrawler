761ac438b6cf0f4331ba32ed07266cfeb4eb67f4
QiwenZhou <452252441@qq.com>
2024-03-17 11:34:45 +0800

    Install Python 3.10.12

--- install-prerequisite.sh
+++ install-prerequisite.sh
@@ -34,15 +34,38 @@ echo "
    Step 2: Install Python and Related Dependencies
 **************************************************
 "
-# install python3
-sudo apt-get update
-sudo apt-get install python3
+## install python3
+#sudo apt-get update
+#sudo apt-get install python3
+
+# install python3.10.12
+wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
+tar -xvf Python-3.10.12.tgz
+cd Python-3.10.12
+
+sudo yum groupinstall "Development Tools"
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel
+
+./configure --enable-optimizations --with-openssl=/usr/include/openssl
+make altinstall
+
 
 cd AeroIntelligenceCrawler
 # create virtual environment
 python3 -m venv crawler
 # activate virtual environment
 source crawler/bin/activate
+# install dependencies
+sudo apt-get install libxml2-devel libxslt-devel
+pip install --upgrade pip
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+
+
+cd AeroIntelligenceCrawler
+# create virtual environment
+python3.10 -m venv crawler_py310
+# activate virtual environment
+source crawler_py310/bin/activate
 # install dependencies
 sudo apt-get install libxml2-devel libxslt-devel
 pip install --upgrade pip
