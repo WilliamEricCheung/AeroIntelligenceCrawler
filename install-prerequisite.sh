5e08faa0bc393cce45b7589aaf2f5232c308976e
Yihui Zhang <zhangyihui@buaa.edu.cn>
2024-03-20 17:39:58 +0800

    fix

--- install-prerequisite.sh
+++ install-prerequisite.sh
@@ -6,10 +6,47 @@
 # example: WORK_DIR="/mnt/d/Project/AeroIntelligenceCrawler"
 WORK_DIR="$(pwd)"
 
+echo "
+**************************************************
+   Step 1: Install Docker
+**************************************************
+"
+
+# https://docs.docker.com/engine/install/ubuntu/
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+
+cat /etc/docker/daemon.json << EOF
+{
+    "registry-mirrors": [
+        "https://hub-mirror.c.163.com",
+        "https://mirror.baidubce.com",
+    ]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
 # install browswer driver (chrome for ubuntu)
 echo "
 ****************************
-   Step 1: Install Chrome
+   Step 2: Install Chrome
 ****************************
 "
 if grep -q -i "debian" /etc/os-release; then
@@ -35,7 +72,7 @@ fi
 
 echo "
 **************************************************
-   Step 2: Install Python and Related Dependencies
+   Step 3: Install Python and Related Dependencies
 **************************************************
 "
 ## install python3
@@ -78,7 +115,7 @@ pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 
 echo "
 **************************************************
-   Step 3: ElasticSearch
+   Step 4: ElasticSearch
 **************************************************
 "
 docker pull elasticsearch:8.12.2
@@ -117,13 +154,14 @@ docker run -d --name es -p 9200:9200 -p 9300:9300 \
 docker exec -it es /bin/bash
 cd /usr/share/elasticsearch/bin
 elasticsearch-plugin install analysis-smartcn
+# 自动安装不行，手动拷贝到/usr/share/elasticsearch/plugins创建ik目录，解压缩到ik
 echo y | elasticsearch-plugin install https://github.com/infinilabs/analysis-ik/releases/download/v8.12.2/elasticsearch-analysis-ik-8.12.2.zip
 exit
 docker restart es
 
 echo "
 **************************************************
-   Step 4: Kibana for ElasticSearch
+   Step 5: Kibana for ElasticSearch
 **************************************************
 "
 docker pull kibana:8.12.2
@@ -134,8 +172,8 @@ docker cp kibana_temp:/usr/share/kibana/config $WORK_DIR/kibana/
 chmod -R 777 $WORK_DIR/kibana
 docker rm -f  kibana_temp
 # change kibana configuration
-sed -i 's/http:\/\/elasticsearch:9200/http:\/\/es:9200/g' $WORK_DIR/kibana/kibana.yml
+sed -i 's/http:\/\/elasticsearch:9200/http:\/\/es:9200/g' $WORK_DIR/kibana/config/kibana.yml
 
 docker run -d --name kibana -p 5601:5601 \
--v $WORK_DIR/kibana/kibana.yml:/usr/share/kibana/config/kibana.yml \
+-v $WORK_DIR/kibana/config/kibana.yml:/usr/share/kibana/config/kibana.yml \
 --network es-net --restart=always kibana:8.12.2
